2049630c2bc9330cd55a2f526a941fbded769d66
Juan Andrés Ruiz Muñoz <143564687+JuanRuizIng@users.noreply.github.com>
2024-11-12 10:18:20 -0500

    Add Comentarios service
    Now are complete comentarios service without validation

--- backend/apiDocsTICS/src/main/java/com/example/apiDocsTICS/Repository/IUsuariosRepository.java
+++ backend/apiDocsTICS/src/main/java/com/example/apiDocsTICS/Repository/IUsuariosRepository.java
@@ -6,10 +6,11 @@ import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.apiDocsTICS.Model.UsuariosModel;
 
-
+@Repository
 public interface IUsuariosRepository extends MongoRepository<UsuariosModel, ObjectId> {
     Optional<UsuariosModel> findByNombreUsuario(String nombreUsuario);
     @Aggregation(pipeline = {
